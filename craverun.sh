dbb3b99b5453c487787f8741b947d50bd246be24
Shravankumar <shravankumarsayz@gmail.com>
2024-03-22 14:43:21 +0530

    Update craverun.sh

--- craverun.sh
+++ craverun.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 rm -rf crave_rom_builder
-git clone https://$GH_TOKEN@github.com/xc112lg/crave_rom_builder.git
+git clone https://$GH_TOKEN@github.com/shravansayz/crave_rom_builder.git
 cp -f crave_rom_builder/pull.sh pull.sh
 chmod +x pull.sh
 crave run --no-patch  -- "          #© crave.io Inc. 2017-2024
